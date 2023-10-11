92357d7768c75443658f7796c18dc282ec527256
Daniel Guilherme <daniel.meloso@outlook.com>
2023-10-11 12:15:21 -0300

    Configuração do DbContext e Identity

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Data;
+using UsuariosApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var coonString = builder.Configuration.GetConnectionString("UsuarioConnection");
+builder.Services.AddDbContext<UsuarioDbContext>( opts =>
+{
+    opts.UseMySql(coonString, ServerVersion.AutoDetect(coonString));
+});
+builder.Services
+    .AddIdentity<Usuario, IdentityRole>()
+    .AddEntityFrameworkStores<UsuarioDbContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
