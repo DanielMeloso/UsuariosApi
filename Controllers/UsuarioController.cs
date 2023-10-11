7394754b3a642842d519724677244c349c0bb5e9
Daniel Guilherme <daniel.meloso@outlook.com>
2023-10-11 11:16:05 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    Route()
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
